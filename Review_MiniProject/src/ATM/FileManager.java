e2e35d4fee9c1c8b25b52043b159e74472aa25ef
sksmsy <sksmsy@localhost>
2020-12-13 22:47:38 +0900

    Review 1213

--- Review_MiniProject/src/ATM/FileManager.java
+++ Review_MiniProject/src/ATM/FileManager.java
@@ -14,9 +14,7 @@ public class FileManager {
 	String data = "";
 	
 	void setData() {
-		data = "";
 		int cnt = UserManager.instance.userCnt;
-		
 		data += cnt;
 		data += "\n";
 		
@@ -35,28 +33,29 @@ public class FileManager {
 					data += UserManager.instance.user[i].acc[j].num;
 					data += "/";
 					data += UserManager.instance.user[i].acc[j].money;
-					if(j != UserManager.instance.user[i].accCnt-1) {
+					if(j != UserManager.instance.user[i].accCnt -1) {
 						data += ",";
 					}
 				}
-				data += "\n";
 			}
+			data += "\n";
 		}
-		
 	}
 	
 	void save() {
 		setData();
 		FileWriter fw = null;
-		
 		try {
 			fw = new FileWriter(fileName);
 			fw.write(data);
+			
 		} catch (Exception e) {
 			e.printStackTrace();
-		}finally {
-			if(fw != null) {try{fw.close();} catch(IOException e) {}}
+		}finally{
+			if(fw != null) { try {fw.close();} catch(IOException e) {}}
 		}
+		
+	
 	}
 	
 	void load() {
@@ -65,71 +64,77 @@ public class FileManager {
 		BufferedReader br = null;
 		
 		try {
+			fr = new FileReader(file);
+			br = new BufferedReader(fr);
 			if(file.exists()) {
-				fr = new FileReader(file);
-				br = new BufferedReader(fr);
+				
 				
 				while(true) {
 					String line = br.readLine();
+					
 					if(line == null) {
 						break;
 					}
 					data += line;
 					data += "\n";
 				}
-			
+				
+				
 				String temp[] = data.split("\n");
-				UserManager.instance.userCnt = Integer.parseInt(temp[0]);
+				UserManager.instance.userCnt = Integer.parseInt(temp[0]); 
 				UserManager.instance.user = new User[UserManager.instance.userCnt];
 				for (int i = 0; i < UserManager.instance.userCnt; i++) {
 					UserManager.instance.user[i] = new User();
 				}
 				
 				int j = 0;
-				for (int i = 1; i < temp.length; i++) {
-					String id= temp[i];
-					String pw= temp[i+1];
+				for (int i = 1; i < temp.length; i+=4) {
+					String id = temp[i];
+					String pw = temp[i+1];
 					int accCnt = Integer.parseInt(temp[i+2]);
-					UserManager.instance.user[j].id = id;
-					UserManager.instance.user[j].pw = pw;
-					UserManager.instance.user[j].accCnt = accCnt;
+					
 					String accInfo = temp[i+3];
+					
 					if(accCnt == 1) {
 						String temp2[] = accInfo.split("/");
-						int acc = Integer.parseInt(temp[0]);
-						int money = Integer.parseInt(temp[1]);
+						int acc = Integer.parseInt(temp2[0]);
+						int money = Integer.parseInt(temp2[1]);
+						
 						UserManager.instance.user[j].acc[0] = new Account();
 						UserManager.instance.user[j].acc[0].num = acc;
 						UserManager.instance.user[j].acc[0].money = money;
+						
 					}
-					if(accCnt > 1) {
+					if(accCnt > 1){
 						String temp2[] = accInfo.split(",");
 						for (int k = 0; k < temp2.length; k++) {
-							String[] parse = temp[k].split("/");
-							int acc = Integer.parseInt(parse[0]);
-							int money = Integer.parseInt(parse[1]);
+							String temp3[] = temp2[k].split("/");
+							
+							int acc = Integer.parseInt(temp3[0]);
+							int money = Integer.parseInt(temp3[1]);
+							
 							UserManager.instance.user[j].acc[k] = new Account();
 							UserManager.instance.user[j].acc[k].num = acc;
 							UserManager.instance.user[j].acc[k].money = money;
 							
 						}
 					}
-					j+=1;
+					j += 1;
 				}
+				
 				
 			}else {
 				UserManager.instance.setdummy();
 				setData();
 				save();
+			
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}finally {
-			if(fr != null) {try {fr.close();} catch (IOException e) {}}
-			if(br != null) {try {br.close();} catch (IOException e) {}}
+			if(fr != null) { try {fr.close();} catch(IOException e) {}}
+			if(br != null) { try {br.close();} catch(IOException e) {}}
 		}
 	}
-	
-	
 	
 }
